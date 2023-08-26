29ced2ee14b49d6d6fbe31674ee893b83f296f06
Roberto Perez <pejir84@gmail.com>
2023-08-26 17:51:16 +0200

    Create twttr.py

---
+++ Problem Set 2_Loops/twttr.py
@@ -0,0 +1,17 @@
+#get user input
+answer = input("Input: ")
+
+#print output
+print("Output: ", end="")
+print("")
+
+#loop through every letter
+for letter in answer:
+
+    #check if it is a vowel or not
+    if not letter.lower() in ['a','e','i','o','u']:
+        print(letter, end="")
+
+
+#print new line
+print()
